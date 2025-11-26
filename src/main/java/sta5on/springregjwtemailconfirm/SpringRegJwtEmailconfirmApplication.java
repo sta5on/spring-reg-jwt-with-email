783cdd449e997fd197257dfd4fb09f03ee26f168
sta5on <stan.voronet@gmail.com>
2025-11-26 13:04:40 +0200

    initial

---
+++ src/main/java/sta5on/springregjwtemailconfirm/SpringRegJwtEmailconfirmApplication.java
@@ -0,0 +1,13 @@
+package sta5on.springregjwtemailconfirm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRegJwtEmailconfirmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRegJwtEmailconfirmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sta5on/springregjwtemailconfirm/SpringRegJwtEmailconfirmApplicationTests.java
@@ -0,0 +1,13 @@
+package sta5on.springregjwtemailconfirm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRegJwtEmailconfirmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
